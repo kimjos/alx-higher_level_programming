4bdbbcb70311d02bb3e845379255b892e10b395f
kimjos <josephkimani500@gmail.com>
2022-12-05 15:16:50 +0000

    modified

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,44 +1,48 @@
-nclude <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
-/**
- * palindrome_rec - Iterates through a list recursively
- * @head: head of the list
- * @tail: iterates to the end of the list
- *
- * Return: 1 if a palindrome, 0 if not a palindrome
- */
-
-int palindrome_rec(listint_t **head, listint_t *tail)
-{
-	if (tail == NULL)
-		return (1);
-
-	if (palindrome_rec(head, tail->next) == 1 && (*head)->n == tail->n)
-	{
-		(*head) = (*head)->next;
-		return (1);
-	}
-
-	else
-		return (0);
-}
-
 
 /**
- * is_palindrome - checks if linked list is a palindrome
- * @head: head of the list
+ * is_palindrome - checks if a sll is a palindrome
+ * @head: head of sll
  *
- * Return: 1 if a palindrome, 0 if not a palindrome
+ * Return: 1 for true, 0 for false
  */
-
 int is_palindrome(listint_t **head)
 {
-	if (*head == NULL)
+	int len = 0;
+	int jump, i, j, len_d_2;
+	listint_t *temp1 = (*head);
+	listint_t *temp2 = (*head);
+
+	if (!(*head))
 		return (1);
 
-	if ((*head)->next == NULL)
+	if (len == 0)
+	{
+		while (temp1->next)
+		{
+			temp1 = temp1->next;
+			len++;
+		}
+		len++;
+		if (temp2->n != temp1->n)
+			return (0);
+	}
+
+	if (len == 1)
 		return (1);
 
-	return (palindrome_rec(head, *head));
+	len_d_2 = len / 2;
+	jump = len - 1 - 2;
+
+	for (i = 1; i < len_d_2; i++, jump -= 2)
+	{
+		temp2 = temp2->next;
+		temp1 = temp2;
+		for (j = 0; j < jump; j++)
+			temp1 = temp1->next;
+		if (temp2->n != temp1->n)
+			return (0);
+	}
+	return (1);
+
 }
