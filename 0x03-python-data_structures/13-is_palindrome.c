e2bac65a9115529f1a1720d76118b2eca763ad3c
kimjos <josephkimani500@gmail.com>
2024-01-20 23:28:34 -0800

    lets try again

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,66 +1,29 @@
 #include "lists.h"
-
 /**
- * reverse_list - reverses a linked list
- * @head: pointer to the head of the linked list
- *
- * Return: pointer to the new head of the reversed list
+ * comparator - compare start and end position
+ * @head: the linked list
+ * @last: the end of the list
+ * Return: 1 true, 0 false
  */
-listint_t *reverse_list(listint_t *head)
+int comparator(listint_t **head, listint_t *last)
 {
-	listint_t *prev = NULL, *current = head, *next = NULL;
-
-	while (current != NULL)
+	if (last == NULL)
+		return (1);
+	if (comparator(head, last->next) && (*head)->n == last->n)
 	{
-		next = current->next;
-		current->next = prev;
-		prev = current;
-		current = next;
+		*head = (*head)->next;
+		return (1);
 	}
-
-	return prev;
+	return (0);
 }
-
 /**
  * is_palindrome - checks if a singly linked list is a palindrome
- * @head: pointer to the head of the linked list
- *
- * Return: 1 if it is a palindrome, 0 otherwise
+ * @head: the linked list
+ * Return: 1 true, 0 false
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *slow = *head, *fast = *head, *second_half, *prev_slow = *head;
-
-	if (*head == NULL || (*head)->next == NULL)
+	if (!head || !(*head))
 		return (1);
-
-	// Find the middle of the linked list using slow and fast pointers
-	while (fast != NULL && fast->next != NULL)
-	{
-		fast = fast->next->next;
-		prev_slow = slow;
-		slow = slow->next;
-	}
-
-	// If the length of the linked list is odd, move slow one step ahead
-	if (fast != NULL)
-		slow = slow->next;
-
-	// Reverse the second half of the linked list
-	second_half = reverse_list(slow);
-
-	// Compare the first half and the reversed second half
-	while (second_half != NULL)
-	{
-		if ((*head)->n != second_half->n)
-			return (0);
-
-		*head = (*head)->next;
-		second_half = second_half->next;
-	}
-
-	// Restore the original linked list
-	reverse_list(prev_slow->next);
-	return (1);
+	return (comparator(head, *head));
 }
-
